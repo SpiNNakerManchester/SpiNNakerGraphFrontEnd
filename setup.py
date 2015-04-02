b9fcdbaf5210696885e543fe5cd8856ff6ae4b57
alan-stokes <alan.barry.stokes@gmail.com>
2015-04-02 15:15:28 +0100

    added what i think is right for pip

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+exec(open("spynnaker_graph_front_end/_version.py").read())
+
+setup(
+    name="sPyNNaker",
+    version=__version__,
+    description="Front end to the SpiNNaker tool chain which uses a basic graph",
+    url="https://github.com/SpiNNakerManchester/SpiNNakerGraphFrontEnd",
+    packages=['spynnaker_graph_front_end',
+              'spynnaker_graph_front_end.models',
+              'spynnaker_graph_front_end.utilities',
+              'spynnaker_graph_front_end.utilities.conf',
+              'spynnaker_graph_front_end.utilities.database',
+              'examples',
+              'examples.heat_demo'],
+    package_data={'examples': ['model_binaries/*.aplx'],
+                  'spynnaker_graph_front_end': ['spiNNakerGraphFrontEnd.cfg'],
+                  'spynnaker_graph_front_end.utilities.conf':
+                      ['spiNNakerGraphFrontEnd.cfg.template']},
+    install_requires=['SpiNNMachine >= 2015.003-alpha-01',
+                      'SpiNNMan >= 2015.003-alpha-01',
+                      'SpiNNaker_PACMAN >= 2015.003-alpha-02',
+                      'SpiNNaker_DataSpecification >= 2015.003-alpha-01',
+                      'SpiNNFrontEndCommon >= 2015.001-alpha-01',
+                      'numpy', 'scipy', 'lxml', 'six']
+)
---
+++ spynnaker_graph_front_end/_version.py
@@ -0,0 +1,3 @@
+__version__ = "2015.001-alpha-01"
+__version_month__ = "March"
+__version_year__ = "2015"
