4b69580e76582d6fabb93bb84651ef281fc4f83b
alan-stokes <alan.barry.stokes@gmail.com>
2016-03-23 11:51:59 +0000

    added a template for easyier useage

---
+++ examples/template/c_code.c
@@ -0,0 +1,236 @@
+
+//! imports
+#include "spin1_api.h"
+#include "common-typedefs.h"
+#include <data_specification.h>
+#include <simulation.h>
+#include <debug.h>
+
+//! control value, which says how many timer ticks to run for before exiting
+static uint32_t simulation_ticks = 0;
+static uint32_t infinite_run = 0;
+static uint32_t time = 0;
+
+//! int as a bool to represent if this simulation should run forever
+static uint32_t infinite_run;
+
+//! The recording flags
+static uint32_t recording_flags = 0;
+
+//! human readable definitions of each region in SDRAM
+typedef enum regions_e {
+    SYSTEM_REGION,
+    TRANSMISSIONS,
+    RECORDED_DATA,
+    BUFFERING_OUT_STATE,
+} regions_e;
+
+//! values for the priority for each callback
+typedef enum callback_priorities{
+    MC_PACKET = -1, SDP = 0, USER = 3, TIMER = 2
+} callback_priorities;
+
+//! human readable definitions of each element in the transmission region
+typedef enum transmission_region_elements {
+    HAS_KEY, MY_KEY
+} transmission_region_elements;
+
+//=============================================================================
+// variables to set
+app_name = ""
+
+
+//=============================================================================
+// packet interfaces
+
+
+//! \brief functionality to add when received a multicast packet without payload.
+//! \param[in] key is the key used in routing
+//! \param[in] payload is the payload of packet [none].
+//! \return None
+void receive_data_no_payload(uint key, uint payload) {
+    use(payload);
+}
+
+//! \brief functionality to add when received a multicast packet with payload.
+//! \param[in] key is the key used in routing
+//! \param[in] payload is the payload of packet.
+//! \return None
+void receive_data_payload(uint key, uint payload) {
+
+}
+
+//=============================================================================
+// timer tick interface
+
+//! \brief functionality to execute within a timer tick callback.
+//! \param[in] ticks: the number of times the timer tick callback has been called.
+//! \param[in] time: the tracked timer.
+//! \return None
+void do_update(uint ticks, uint32_t time){
+
+}
+
+//=============================================================================
+//Recording interfaces
+
+//! \brief returns the number fo recording regions used by this model
+//! \return the number of recorded regions to use.
+int number_of_recorded_regions(){
+  return 1;
+}
+
+//! \brief returns the ids in an array for recording to work
+//! \return the arrays of ints for each recorded region id
+region_e * region_ids_for_recording(){
+  regions_e regions_ids_to_record[] = {RECORDED_DATA};
+  return regions_ids_to_record;
+}
+
+//=============================================================================
+//resume interfaces
+
+//! \brief add functionality to do when your about to resume.
+//! \return None.
+void resume_callback() {
+
+}
+
+
+//==============================================================================
+//==============================================================================
+//==============================================================================
+//==============================================================================
+//==============================Boiler plate code below=========================
+//==============================================================================
+//==============================================================================
+//==============================================================================
+//==============================================================================
+
+
+//! \brief Initialises the recording parts of the model
+//! \return True if recording initialisation is successful, false otherwise
+static bool initialise_recording(){
+    address_t address = data_specification_get_data_address();
+    address_t system_region = data_specification_get_region(
+        SYSTEM_REGION, address);
+    uint32_t *recording_flags_from_system_conf =
+        &system_region[SIMULATION_N_TIMING_DETAIL_WORDS];
+    regions_e state_region = BUFFERING_OUT_STATE;
+
+    bool success = recording_initialize(
+        number_of_recorded_regions(), region_ids_for_recording(),
+        recording_flags_from_system_conf, state_region, TIMER,
+        &recording_flags);
+    log_info("Recording flags = 0x%08x", recording_flags);
+    return success;
+}
+
+//! \brief timer tick callback functionality
+//! param[in] ticks: the number of times this has been called
+//! param[in] b: no idea
+//! \return None
+void update(uint ticks, uint b) {
+    use(b);
+    use(ticks);
+
+    time++;
+
+    log_info("on tick %d", time);
+    log_info("sim ticks %d", simulation_ticks);
+
+    // check that the run time hasn't already elapsed and thus needs to be
+    // killed
+    if ((infinite_run != TRUE) && (time >= simulation_ticks)) {
+        log_info("Simulation complete.\n");
+
+        // falls into the pause resume mode of operating
+        simulation_handle_pause_resume(resume_callback);
+
+        if (recording_flags > 0) {
+            log_info("updating recording regions");
+            recording_finalise();
+        }
+
+        return;
+    }
+
+    do_update(ticks, time)
+
+}
+
+//! \brief sets up state variables for the system
+//! \param[in] timer_period: pointer to the time between timer tick callbacks
+//! \return: bool which states if it succeed or not
+static bool initialize(uint32_t *timer_period) {
+    log_info("Initialise: started\n");
+
+    // Get the address this core's DTCM data starts at from SRAM
+    address_t address = data_specification_get_data_address();
+
+    // Read the header
+    if (!data_specification_read_header(address)) {
+        log_error("failed to read the data spec header");
+        return false;
+    }
+
+    // Get the timing details
+    address_t system_region = data_specification_get_region(
+        SYSTEM_REGION, address);
+    if (!simulation_read_timing_details(
+            system_region, APPLICATION_NAME_HASH, timer_period)) {
+        log_error("failed to read the system header");
+        return false;
+    }
+
+    // initialise transmission keys
+    address_t transmission_region_address = data_specification_get_region(
+            TRANSMISSIONS, address);
+    if (transmission_region_address[HAS_KEY] == 1) {
+        my_key = transmission_region_address[MY_KEY];
+        log_info("my key is %d\n", my_key);
+    }
+
+    return true;
+}
+
+//! \brief main entrance method for the model
+//! return None
+void c_main() {
+    log_info("starting %s\n", app_name);
+
+    // Load DTCM data
+    uint32_t timer_period;
+
+    // initialise the model
+    if (!initialize(&timer_period)) {
+        rt_error(RTE_SWERR);
+    }
+
+    // initialise the recording section
+    // set up recording data structures
+    if(!initialise_recording()){
+         rt_error(RTE_SWERR);
+    }
+
+    // set timer tick value to configured value
+    log_info("setting timer to execute every %d microseconds", timer_period);
+    spin1_set_timer_tick(timer_period);
+
+    // register callbacks
+    spin1_callback_on(MCPL_PACKET_RECEIVED, receive_data_payload, MC_PACKET);
+    spin1_callback_on(MC_PACKET_RECEIVED, receive_data_no_payload, MC_PACKET);
+    spin1_callback_on(TIMER_TICK, update, TIMER);
+
+    // Set up callback listening to SDP messages
+    simulation_register_simulation_sdp_callback(
+        &simulation_ticks, &infinite_run, SDP);
+
+    // start execution
+    log_info("Starting\n");
+
+    // Start the time at "-1" so that the first tick will be 0
+    time = UINT32_MAX;
+
+    simulation_run();
+}
