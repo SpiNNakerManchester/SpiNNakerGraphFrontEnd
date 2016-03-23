88a6a8d6f9a3e4a1fc12619488c7ee0a717e0ca4
alan-stokes <alan.barry.stokes@gmail.com>
2016-03-23 11:24:55 +0000

    fixed hello world

--- examples/hello_world/hello_world.c
+++ examples/hello_world/hello_world.c
@@ -1,61 +1,71 @@
-/****a* hello_world.c/hello_world_summary
-*
-* COPYRIGHT
-*  Copyright (c) The University of Manchester, 2011. All rights reserved.
-*  SpiNNaker Project
-*  Advanced Processor Technologies Group
-*  School of Computer Science
-*  Author: Arthur Ceccotti
-*******/
 
+//! imports
 #include "spin1_api.h"
 #include "common-typedefs.h"
-#include "recording.h"
 #include <data_specification.h>
+#include <simulation.h>
 #include <debug.h>
 
+//! control value, which says how many timer ticks to run for before exiting
+static uint32_t simulation_ticks = 0;
+static uint32_t infinite_run = 0;
+static uint32_t time = 0;
+
+//! int as a bool to represent if this simulation should run forever
+static uint32_t infinite_run;
+
+//! The recording flags
+static uint32_t recording_flags = 0;
+
+//! the unique identifier of this model, so that it can tell if the data its
+//! reading is for itself.
+#define APPLICATION_MAGIC_NUMBER 0x99b1ff8f
+
+//! human readable definitions of each region in SDRAM
 typedef enum regions_e {
-    SYSTEM_REGION, STRING_DATA_REGION
+    SYSTEM_REGION,
+    RECORDED_DATA,
+    BUFFERING_OUT_STATE,
 } regions_e;
 
-void record_data() {
-    log_info("Recording data...");
+//! values for the priority for each callback
+typedef enum callback_priorities{
+    MC_PACKET = -1, SDP = 0, USER = 3, TIMER = 2
+} callback_priorities;
 
-    uint chip = spin1_get_chip_id();
+//! human readable definitions of each element in the transmission region
+typedef enum transmission_region_elements {
+    HAS_KEY, MY_KEY
+} transmission_region_elements;
 
-    // TODO little hack which only works on 4 chip boards...
-    char* chip_chars;
-    if(chip == 0)       { chip_chars = "0, 0";}
-    else if(chip == 1)  { chip_chars = "0, 1";}
-    else if(chip == 256){ chip_chars = "1, 0";}
-    else if(chip == 257){ chip_chars = "1, 1";}
-    else                { chip_chars = "?, ?";}
 
-    uint core = spin1_get_core_id();
-
-    log_info("Issuing 'Hello World' from chip %d (%s), core %d", chip, chip_chars, core);
-
-    // TODO quick and DIRTY hack to convert a 2 digit int into a string. Using sprintf breaks the DTCM...
-    char core_chars[2] = { core > 9 ? (core/10) + '0' : ' ', core > 9 ? (core%10) + '0' : core + '0' };
-
-    char hello_me[50];
-    hello_me[0] = '\0';
-    strcat(hello_me, "Hello World from ");
-    strcat(hello_me, chip_chars);
-    strcat(hello_me, ", ");
-    strcat(hello_me, core_chars);
-
-    bool recorded = recording_record(e_recording_channel_spike_history,
-                                     hello_me, strlen(hello_me) * sizeof(char));
-
-    if(recorded){ log_info("Hello World recorded successfully!"); }
-    else        { log_error("Hello World was not recorded...");   }
+/****f* heat_demo.c/receive_data
+ *
+ * SUMMARY
+ *  This function is used as a callback for packet received events.
+ * receives data from 4 (NSEW) neighbours and updates the checklist
+ *
+ * SYNOPSIS
+ *  void receive_data (uint key, uint payload)
+ *
+ * INPUTS
+ *   uint key: packet routing key - provided by the RTS
+ *   uint payload: packet payload - provided by the RTS
+ *
+ * SOURCE
+ */
+void receive_data(uint key, uint payload) {
+    use(key);
+    use(payload);
 }
 
 void iobuf_data(){
-    address_t address = data_specification_get_data_address();
 
-    address_t hello_world_address = data_specification_get_region(STRING_DATA_REGION, address);
+    log_info("getting app table address");
+    address_t address = data_specification_get_data_address();
+    log_info("getting recorded region id");
+    address_t hello_world_address =
+        data_specification_get_region(RECORDED_DATA, address);
 
     log_info("Hello world address is %08x", hello_world_address);
 
@@ -63,60 +73,171 @@ void iobuf_data(){
     log_info("Data read is: %s", my_string);
 }
 
-static bool initialize() {
+void record_data() {
+    log_info("Recording data...");
+
+    uint chip = spin1_get_chip_id();
+
+    uint core = spin1_get_core_id();
+
+    log_info("Issuing 'Hello World' from chip %d, core %d",
+             chip, core);
+
+    bool recorded = recording_record(
+        0, "Hello world", 11 * sizeof(char));
+
+    if(recorded){
+        log_info("Hello World recorded successfully!");
+    }
+    else{
+        log_error("Hello World was not recorded...");
+    }
+}
+
+
+//! \brief Initialises the recording parts of the model
+//! \return True if recording initialisation is successful, false otherwise
+static bool initialise_recording(){
+    address_t address = data_specification_get_data_address();
+    address_t system_region = data_specification_get_region(
+        SYSTEM_REGION, address);
+    regions_e regions_to_record[] = {RECORDED_DATA};
+    uint8_t n_regions_to_record = 1;
+    uint32_t *recording_flags_from_system_conf =
+        &system_region[SIMULATION_N_TIMING_DETAIL_WORDS];
+    regions_e state_region = BUFFERING_OUT_STATE;
+
+    bool success = recording_initialize(
+        n_regions_to_record, regions_to_record,
+        recording_flags_from_system_conf, state_region, TIMER,
+        &recording_flags);
+    log_info("Recording flags = 0x%08x", recording_flags);
+    return success;
+}
+
+void resume_callback() {
+    time = UINT32_MAX;
+}
+
+/****f* heat_demo.c/update
+ *
+ * SUMMARY
+ *
+ * SYNOPSIS
+ *  void update (uint ticks, uint b)
+ *
+ * SOURCE
+ */
+void update(uint ticks, uint b) {
+    use(b);
+    use(ticks);
+
+    time++;
+
+    log_info("on tick %d", time);
+    log_info("sim ticks %d", simulation_ticks);
+
+    // check that the run time hasn't already elapsed and thus needs to be
+    // killed
+    if ((infinite_run != TRUE) && (time >= simulation_ticks)) {
+        log_info("Simulation complete.\n");
+
+        if (recording_flags > 0) {
+            log_info("updating recording regions");
+            recording_finalise();
+        }
+
+        // falls into the pause resume mode of operating
+        simulation_handle_pause_resume(resume_callback);
+
+        return;
+
+    }
+    if(time == 1){
+        record_data();
+    }
+    else if(time ==  100){
+        iobuf_data();
+    }
+
+    // trigger buffering_out_mechanism
+    log_info("reocrding flags is %d", recording_flags);
+    if (recording_flags > 0) {
+        log_info("doing timer tick update\n");
+        recording_do_timestep_update(time);
+        log_info("done timer tick update\n");
+    }
+}
+
+static bool initialize(uint32_t *timer_period) {
+    log_info("Initialise: started\n");
 
     // Get the address this core's DTCM data starts at from SRAM
     address_t address = data_specification_get_data_address();
-    
-    log_info("DataSpec data address is %08x", address);
 
     // Read the header
     if (!data_specification_read_header(address)) {
-        log_error("Could not read DataSpec header");
+        log_error("failed to read the data spec header");
         return false;
     }
 
-    address_t system_region = data_specification_get_region(SYSTEM_REGION, address);
-    address_t data_region   = data_specification_get_region(STRING_DATA_REGION, address);
-
-    log_info("System region: %08x", system_region);
-    log_info("Data region: %08x", data_region);
-
-    uint32_t data_region_size = 500;
-
-                                               // TODO should make my own channel
-    if (!recording_initialse_channel(data_region, e_recording_channel_spike_history, data_region_size)) {
-        log_error("Could not initialize channel.");
+    // Get the timing details
+    address_t system_region = data_specification_get_region(
+        SYSTEM_REGION, address);
+    if (!simulation_read_timing_details(
+            system_region, APPLICATION_MAGIC_NUMBER, timer_period)) {
+        log_error("failed to read the system header");
         return false;
     }
 
-    log_info("Initialization completed successfully!");
     return true;
 }
 
-void update (uint ticks, uint b)
-{
-    // I give it a few ticks between reading and writing, just in case
-    // the IO operations take a bit of time
-    if(ticks == 1)          { record_data(); }
-    else if(ticks == 100)   { iobuf_data();  }
-    else if(ticks == 200)   { recording_finalise();
-                              spin1_exit(0);}
-}
+/****f* heat_demo.c/c_main
+ *
+ * SUMMARY
+ *  This function is called at application start-up.
+ *  It is used to register event callbacks and begin the simulation.
+ *
+ * SYNOPSIS
+ *  int c_main()
+ *
+ * SOURCE
+ */
+void c_main() {
+    log_info("starting heat_demo\n");
 
-void c_main()
-{
-    log_info("Initializing Distributed Hello World...");
+    // Load DTCM data
+    uint32_t timer_period;
 
-    if (!initialize()) {
+    // initialise the model
+    if (!initialize(&timer_period)) {
         rt_error(RTE_SWERR);
     }
 
-    // set timer tick value to 100ms
-    spin1_set_timer_tick(100);
+    // initialise the recording section
+    // set up recording data structures
+    if(!initialise_recording()){
+         rt_error(RTE_SWERR);
+    }
+
+    // set timer tick value to configured value
+    log_info("setting timer to execute every %d microseconds", timer_period);
+    spin1_set_timer_tick(timer_period);
 
     // register callbacks
-    spin1_callback_on (TIMER_TICK, update, 0);
+    spin1_callback_on(MCPL_PACKET_RECEIVED, receive_data, MC_PACKET);
+    spin1_callback_on(TIMER_TICK, update, TIMER);
+
+    // Set up callback listening to SDP messages
+    simulation_register_simulation_sdp_callback(
+        &simulation_ticks, &infinite_run, SDP);
+
+    // start execution
+    log_info("Starting\n");
+
+    // Start the time at "-1" so that the first tick will be 0
+    time = UINT32_MAX;
 
     simulation_run();
 }
--- examples/hello_world/hello_world.py
+++ examples/hello_world/hello_world.py
@@ -5,40 +5,38 @@ Each core stores into its region in SDRAM the string:
 "Hello World from $chip.x, $chip.y, $core"
 
 We then fetch the written data and print it on the python console.
-
-Author: Arthur Ceccotti
 """
 
-import spynnaker_graph_front_end as front_end
+import spinnaker_graph_front_end as front_end
 
 from hello_world_vertex import HelloWorldVertex
 
-import logging
+# import the folder where all graph front end binaries are located
+from examples import model_binaries
 
-import memory_reader
-import os
+import logging
 
 logger = logging.getLogger(__name__)
 
 front_end.setup(graph_label="hello_world",
-                model_binary_module=file(os.getcwd() + "/../model_binaries/hello_world.aplx"))
+                model_binary_module=model_binaries)
 
-dimenions = front_end.get_machine_dimensions()
+dimensions = front_end.get_machine_dimensions()
 
-machine_time_step = 100
+machine_time_step = 1000
 time_scale_factor = 1
 
-machine_port = 11111
-machine_recieve_port = 22222
-machine_host = "0.0.0.0"
+x_dimension = dimensions['x'] + 1
+y_dimension = dimensions['y'] + 1
 
-x_dimension = dimenions['x']+1
-y_dimension = dimenions['y']+1
-p_dimension = 18
+x_dimension = 2
+y_dimension = 2
+
+p_dimension = 16
 
 # calculate total number of 'free' cores for the given board
 # (ie. does not include those busy with sark or reinjection)
-total_number_of_cores = (p_dimension-2) * x_dimension * y_dimension
+total_number_of_cores = (p_dimension - 2) * x_dimension * y_dimension
 
 # fill all cores with a HelloWorldVertex each
 for x_position in range(0, total_number_of_cores):
@@ -46,15 +44,21 @@ for x_position in range(0, total_number_of_cores):
             HelloWorldVertex,
             {'machine_time_step': machine_time_step,
              'time_scale_factor': time_scale_factor},
-             label="Hello World at x_position {}".format(x_position))
+            label="Hello World at x_position {}".format(x_position))
 
 front_end.run(10)
 
+placements = front_end.placements()
+buffer_manager = front_end.buffer_manager()
+# read out sdram
 for x in range(0, x_dimension):
     for y in range(0, y_dimension):
         for p in range(0, p_dimension):
-            hello_world = memory_reader.read(transceiver=front_end.get_txrx(), x=x, y=y, p=p,
-                                            recording_region = HelloWorldVertex.DATA_REGIONS.STRING_DATA.value)
-            logger.info("{}, {}, {} > {}".format(x, y, p, hello_world))
+            if placements.is_subvertex_on_processor(x, y, p):
+                vertex = placements.get_subvertex_on_processor(x, y, p)
+                hello_world = vertex.read(
+                    placement=placements.get_placement_of_subvertex(vertex),
+                    buffer_manager=buffer_manager)
+                logger.info("{}, {}, {} > {}".format(x, y, p, hello_world))
 
 front_end.stop()
