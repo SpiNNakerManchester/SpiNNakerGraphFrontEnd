a5e8a594544b1e4bff5b85cd425ff4970630d445
alan-stokes <alan.barry.stokes@gmail.com>
2017-01-05 10:56:58 +0000

    added ability to force router compressor to compress anyhows.
    Added ability to control compress nature and reading iobuf by cfg params
    
    added log infos and log errors to the router compressor to support better intergration into the tools. and for debug purposes

--- spinnaker_graph_front_end/spinnaker.py
+++ spinnaker_graph_front_end/spinnaker.py
@@ -32,6 +32,13 @@ class SpiNNaker(SpinnakerMainInterface):
         extra_mapping_inputs = dict()
         extra_mapping_inputs["CreateAtomToEventIdMapping"] = config.getboolean(
             "Database", "create_routing_info_to_atom_id_mapping")
+        extra_mapping_inputs['RecordIOBufOnChipRouterCompressorFlag'] = \
+            config.getboolean("OnChipRouterCompressor",
+                              "record_iobuf_from_on_chip_router_compressor")
+        extra_mapping_inputs['RouterCompressorOnlyCompressWhenNeededFlag'] = \
+            config.getboolean(
+                "OnChipRouterCompressor",
+                "on_chip_router_compressor_compress_only_when_needed")
 
         SpinnakerMainInterface.__init__(
             self, config, graph_label=graph_label,
