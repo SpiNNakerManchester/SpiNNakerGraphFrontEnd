cafa6e931918a1f36a7a3c88dc66c191c0c07421
Christian <brenninc@cs.man.ac.uk>
2017-02-21 09:29:30 +0000

    skipped whole config if in readthedocs

--- spinnaker_graph_front_end/utilities/conf/__init__.py
+++ spinnaker_graph_front_end/utilities/conf/__init__.py
@@ -18,14 +18,8 @@ import spinnaker_graph_front_end
 from spinnaker_graph_front_end.utilities.conf.log import ConfiguredFilter
 from spinnaker_graph_front_end.utilities.conf.log import ConfiguredFormatter
 
-read = list()
-
 
 def _install_cfg():
-    if os.environ.get('READTHEDOCS', None) == 'True':
-        print "config file creation prevented as in readthedocs"
-        return
-
     template_cfg = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                 "spiNNakerGraphFrontEnd.cfg.template")
     home_cfg = os.path.expanduser("~/.spiNNakerGraphFrontEnd.cfg")
@@ -42,39 +36,6 @@ def _install_cfg():
     print "************************************"
     sys.exit(0)
 
-# Create a config, read global defaults and then read in additional files
-config = ConfigParser.RawConfigParser()
-default = os.path.join(
-    os.path.dirname(spinnaker_graph_front_end.__file__),
-    "spiNNakerGraphFrontEnd.cfg")
-user_config = os.path.expanduser(os.path.join(
-    "~", ".spiNNakerGraphFrontEnd.cfg"))
-other_configs = (user_config, "spiNNakerGraphFrontEnd.cfg")
-located_configs = list()
-
-found_config = False
-for possible_config in other_configs:
-    if os.path.isfile(possible_config):
-        found_config = True
-        located_configs.append(os.path.abspath(possible_config))
-
-with open(default) as f:
-    config.readfp(f)
-if found_config:
-    read = config.read(other_configs)
-else:
-    # Create a default pacman.cfg in the user home directory and get them
-    # to update it.
-    _install_cfg()
-
-read.append(default)
-
-machine_spec_file_path = config.get("Machine", "machine_spec_file")
-if machine_spec_file_path != "None":
-    config.read(machine_spec_file_path)
-    read.append(machine_spec_file_path)
-
-
 # creates a directory if needed, or deletes it and rebuilds it
 def create_directory(directory):
     if not os.path.exists(directory):
@@ -83,21 +44,57 @@ def create_directory(directory):
         shutil.rmtree(directory)
         os.makedirs(directory)
 
+if os.environ.get('READTHEDOCS', None) == 'True':
+    print "config creation prevented as in readthedocs"
+else:
+    read = list()
 
-# Create the root logger with the given level
-# Create filters based on logging levels
-try:
-    if config.getboolean("Logging", "instantiate"):
-        logging.basicConfig(level=0)
+    # Create a config, read global defaults and then read in additional files
+    config = ConfigParser.RawConfigParser()
+    default = os.path.join(
+        os.path.dirname(spinnaker_graph_front_end.__file__),
+        "spiNNakerGraphFrontEnd.cfg")
+    user_config = os.path.expanduser(os.path.join(
+        "~", ".spiNNakerGraphFrontEnd.cfg"))
+    other_configs = (user_config, "spiNNakerGraphFrontEnd.cfg")
+    located_configs = list()
 
-    for handler in logging.root.handlers:
-        handler.addFilter(ConfiguredFilter(config))
-        handler.setFormatter(ConfiguredFormatter(config))
-except ConfigParser.NoSectionError:
-    pass
-except ConfigParser.NoOptionError:
-    pass
+    found_config = False
+    for possible_config in other_configs:
+        if os.path.isfile(possible_config):
+            found_config = True
+            located_configs.append(os.path.abspath(possible_config))
 
-# Log which config files we read
-logger = logging.getLogger(__name__)
-logger.info("Read config files: %s" % string.join(read, ", "))
+    with open(default) as f:
+        config.readfp(f)
+    if found_config:
+        read = config.read(other_configs)
+    else:
+        # Create a default pacman.cfg in the user home directory and get them
+        # to update it.
+        _install_cfg()
+
+    read.append(default)
+
+    machine_spec_file_path = config.get("Machine", "machine_spec_file")
+    if machine_spec_file_path != "None":
+        config.read(machine_spec_file_path)
+        read.append(machine_spec_file_path)
+
+    # Create the root logger with the given level
+    # Create filters based on logging levels
+    try:
+        if config.getboolean("Logging", "instantiate"):
+            logging.basicConfig(level=0)
+
+        for handler in logging.root.handlers:
+            handler.addFilter(ConfiguredFilter(config))
+            handler.setFormatter(ConfiguredFormatter(config))
+    except ConfigParser.NoSectionError:
+        pass
+    except ConfigParser.NoOptionError:
+        pass
+
+    # Log which config files we read
+    logger = logging.getLogger(__name__)
+    logger.info("Read config files: %s" % string.join(read, ", "))
