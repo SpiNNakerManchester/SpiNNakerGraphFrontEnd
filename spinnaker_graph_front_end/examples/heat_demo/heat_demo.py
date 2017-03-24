48b60cb49792389bdec2fca92674a6ac7ea9800b
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2017-03-24 22:45:59 +0000

    Reverted switch

--- spinnaker_graph_front_end/examples/heat_demo/heat_demo.py
+++ spinnaker_graph_front_end/examples/heat_demo/heat_demo.py
@@ -84,8 +84,8 @@ for chip in machine.ethernet_connected_chips:
 max_x_element_id = (machine.max_chip_x + 1) * 4
 max_y_element_id = (machine.max_chip_y + 1) * 4
 vertices = [
-    [None for j in range(max_x_element_id)]
-    for i in range(max_y_element_id)
+    [None for j in range(max_y_element_id)]
+    for i in range(max_x_element_id)
 ]
 
 receive_labels = list()
