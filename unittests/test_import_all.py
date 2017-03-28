0ab260467e4fda31ba28cb6b24d3338e0c89d95f
Christian <brenninc@cs.man.ac.uk>
2017-03-28 14:46:56 +0100

    pep8

--- unittests/test_import_all.py
+++ unittests/test_import_all.py
@@ -6,16 +6,23 @@ import spinn_utilities.package_loader as package_loader
 class ImportAllModule(unittest.TestCase):
 
     BROKEN = ["spinnaker_graph_front_end.examples.heat_demo.heat_demo",
+
               "spinnaker_graph_front_end.examples.Conways."
               "1_no_graph_example.conways_no_graph",
+
               "spinnaker_graph_front_end.examples."
               "Conways.no_edges_examples.conways_no_edges",
+
               "spinnaker_graph_front_end.examples.Conways."
               "partitioned_example_a_no_vis_no_buffer.conways_partitioned",
+
               "spinnaker_graph_front_end.examples."
-              "Conways.partitioned_example_b_no_vis_buffer.conways_partitioned",
+              "Conways.partitioned_example_b_no_vis_buffer."
+              "conways_partitioned",
+
               "spinnaker_graph_front_end.examples."
               "Conways.partitioned_example_c_vis_buffer.conways_partitioned",
+
               "spinnaker_graph_front_end.examples.hello_world.hello_world"]
 
     def test_import_all(self):
