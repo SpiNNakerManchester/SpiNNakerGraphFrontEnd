cee8274bcfab564afd99618ae4dbe728d9690ca6
Christian <brenninc@cs.man.ac.uk>
2017-05-03 16:56:12 +0100

    bumped version and unified where it came from

--- spinnaker_graph_front_end/_version.py
+++ spinnaker_graph_front_end/_version.py
@@ -1,4 +1,4 @@
-__version__ = "3.0.0"
-__version_month__ = "September"
-__version_year__ = "2016"
-__version_name__ = ""
+__version__ = "1!4.0.0a5"
+__version_month__ = "May"
+__version_year__ = "2017"
+__version_name__ = "Riptalon"
