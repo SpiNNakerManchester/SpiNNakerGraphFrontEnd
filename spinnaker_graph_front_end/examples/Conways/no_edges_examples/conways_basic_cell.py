c51bccc28b6a168e370ddbfcb5abe701efcfe33c
Andrew Gait <andrew.gait@manchester.ac.uk>
2017-08-02 15:25:04 +0100

    another flake8

--- spinnaker_graph_front_end/examples/Conways/no_edges_examples/conways_basic_cell.py
+++ spinnaker_graph_front_end/examples/Conways/no_edges_examples/conways_basic_cell.py
@@ -1,6 +1,7 @@
 from pacman.model.graphs.machine import MachineVertex
 from pacman.model.resources import ResourceContainer
 
+
 class ConwayBasicCell(MachineVertex):
     """ Cell which represents a cell within the 2d fabric
     """
