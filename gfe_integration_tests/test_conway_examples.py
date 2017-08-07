dd3ff41f215fde669f11f0f03f18f683ddb0b5ea
Christian Brenninkmeijer <brenninc@cs.man.ac.uk>
2017-08-07 12:55:49 +0100

    cleaned conway to be more scripts yet still test ok

--- gfe_integration_tests/test_conway_examples.py
+++ gfe_integration_tests/test_conway_examples.py
@@ -1,34 +1,44 @@
-from spinnaker_graph_front_end.examples.Conways.no_edges_examples \
-    import conways_no_edges
-from spinnaker_graph_front_end.examples.Conways.one_no_graph_example \
-    import conways_no_graph
-from spinnaker_graph_front_end.examples.Conways.\
-    partitioned_example_a_no_vis_no_buffer \
-    import conways_partitioned as conways_partitioned_a
-from spinnaker_graph_front_end.examples.Conways.\
-    partitioned_example_b_no_vis_buffer \
-    import conways_partitioned as conways_partitioned_b
-from spinnaker_graph_front_end.examples.\
-    Conways.partitioned_example_c_vis_buffer \
-    import conways_partitioned as conways_partitioned_c
-
-
+import os
 import unittest
 
 
 class TestConwayExamples(unittest.TestCase):
 
     def test_one_no_graph(self):
-        conways_no_graph.do_run()
+        import spinnaker_graph_front_end.examples.Conways.\
+            no_edges_examples.conways_basic_cell as o
+        o.__file__
 
     def test_conways_no_edges(self):
-        conways_no_edges.do_run()
+        import spinnaker_graph_front_end.examples.Conways.\
+            one_no_graph_example as conways_ng
+        class_file = conways_ng.__file__
+        path = os.path.dirname(os.path.abspath(class_file))
+        print path
+        os.chdir(path)
+        import spinnaker_graph_front_end.examples.Conways.\
+            one_no_graph_example.conways_no_graph as n
+        n.__file__
 
     def test_conways_partitioned_a(self):
-        conways_partitioned_a.do_run()
+        import spinnaker_graph_front_end.examples.Conways.\
+            partitioned_example_a_no_vis_no_buffer as conways_a
+        class_file = conways_a.__file__
+        path = os.path.dirname(os.path.abspath(class_file))
+        print path
+        os.chdir(path)
+        import spinnaker_graph_front_end.examples.Conways.\
+            partitioned_example_a_no_vis_no_buffer.conways_partitioned as a
+        # just to avoid import not used NOQA does not work on two lines
+        a.__file__
 
     def test_conways_partitioned_b(self):
-        conways_partitioned_b.do_run()
-
-    def test_conways_partitioned_c(self):
-        conways_partitioned_c.run_broken()
+        import spinnaker_graph_front_end.examples.Conways.\
+            partitioned_example_b_no_vis_buffer as conways_b
+        class_file = conways_b.__file__
+        path = os.path.dirname(os.path.abspath(class_file))
+        os.chdir(path)
+        import spinnaker_graph_front_end.examples.Conways.\
+            partitioned_example_b_no_vis_buffer.conways_partitioned as b
+        # just to avoid import not used NOQA does not work on two lines
+        b.__file__
--- spinnaker_graph_front_end/examples/Conways/one_no_graph_example/conways_no_graph.py
+++ spinnaker_graph_front_end/examples/Conways/one_no_graph_example/conways_no_graph.py
@@ -1,16 +1,8 @@
 import spinnaker_graph_front_end as front_end
 
 
-# spinn_front_end_common.utilities.exceptions.ConfigurationException:
-# There needs to be a graph which contains at least one vertex for
-# the tool chain to map anything.
-def do_run():
-    # set up the front end and ask for the detected machines dimensions
-    front_end.setup()
+# set up the front end and ask for the detected machines dimensions
+front_end.setup()
 
-    front_end.run()
-    front_end.stop()
-
-
-if __name__ == '__main__':
-    do_run()
+front_end.run()
+front_end.stop()
