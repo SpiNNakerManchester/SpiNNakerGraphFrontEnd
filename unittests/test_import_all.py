fe123404593830926f929344f21ea4bc05706d4e
alan-stokes <alan.barry.stokes@gmail.com>
2017-11-13 14:53:59 +0000

    atetmpt to stop it from looking in examples

--- unittests/test_import_all.py
+++ unittests/test_import_all.py
@@ -18,7 +18,8 @@ EXCLUSIONS = ["spinnaker_graph_front_end.examples.Conways."
               "partitioned_example_b_no_vis_buffer.conways_partitioned",
 
               "spinnaker_graph_front_end.examples.hello_world.hello_world",
-              "spinnaker_graph_front_end.examples.template.python_template"]
+              "spinnaker_graph_front_end.examples.template.python_template",
+              "spinnaker_graph_front_end.examples"]
 
 
 class ImportAllModule(unittest.TestCase):
