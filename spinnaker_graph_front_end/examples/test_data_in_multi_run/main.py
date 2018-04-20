755cd8429cd14d366b805c744c60340dff8e8d69
alan-stokes <alan.barry.stokes@gmail.com>
2018-04-20 13:54:50 +0100

    bits of a java run

--- spinnaker_graph_front_end/examples/test_data_in_multi_run/main.py
+++ spinnaker_graph_front_end/examples/test_data_in_multi_run/main.py
@@ -114,7 +114,7 @@ class Runner(object):
 if __name__ == "__main__":
 
     # entry point for doing speed search
-    data_sizes = [1]
+    data_sizes = [20]
     # data_sizes = [1, 2, 5, 10, 20, 30, 50, 100]
     locations = [(7, 7)]
     # locations = [(0, 0), (1, 1), (0, 3), (2, 4), (4, 0), (7, 7)]
