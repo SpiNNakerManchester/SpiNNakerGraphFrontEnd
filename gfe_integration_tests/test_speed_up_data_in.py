0eb98d9e146505718f2eeb36f3812f935c94d956
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2018-05-04 16:24:28 +0100

    print(..)

--- gfe_integration_tests/test_speed_up_data_in.py
+++ gfe_integration_tests/test_speed_up_data_in.py
@@ -9,6 +9,6 @@ class TestDataSpeedUpExamples(unittest.TestCase):
             test_data_in_speed_up_test_multi_board_run as test
         class_file = test.__file__
         path = os.path.dirname(os.path.abspath(class_file))
-        print path
+        print(path)
         os.chdir(path)
         import spinnaker_graph_front_end.examples.test_data_in_speed_up_test_multi_board_run.main  # NOQA
