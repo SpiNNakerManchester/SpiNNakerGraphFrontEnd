1420eac19d528815f69ff9071fb9fb3a34d36ad8
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2018-05-08 11:37:56 +0100

    updated test to convertor

--- gfe_integration_tests/test_rte/test_rte_during_run.mk
+++ gfe_integration_tests/test_rte/test_rte_during_run.mk
@@ -1,8 +1,6 @@
 APP = test_rte_during_run
-APP_OUTPUT_DIR = ./
-BUILD_DIR = build/
-SOURCE_DIRS += $(abspath .)
 SOURCES = test_rte_during_run.c
 
-# The spinnaker_tools standard makefile
-include $(SPINN_DIRS)/make/Makefile.SpiNNFrontEndCommon
+APP_OUTPUT_DIR := $(abspath $(dir $(abspath $(lastword $(MAKEFILE_LIST)))))/
+
+include $(SPINN_DIRS)/make/local.mk
--- gfe_integration_tests/test_rte/test_run_too_long.mk
+++ gfe_integration_tests/test_rte/test_run_too_long.mk
@@ -1,8 +1,6 @@
 APP = test_run_too_long
-APP_OUTPUT_DIR = ./
-BUILD_DIR = build/
-SOURCE_DIRS += $(abspath .)
 SOURCES = test_run_too_long.c
 
-# The spinnaker_tools standard makefile
-include $(SPINN_DIRS)/make/Makefile.SpiNNFrontEndCommon
+APP_OUTPUT_DIR := $(abspath $(dir $(abspath $(lastword $(MAKEFILE_LIST)))))/
+
+include $(SPINN_DIRS)/make/local.mk
