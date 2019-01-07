8c03994cbedf36dcfc7174f460f16c150e26bd62
Andrew Gait <andrew.gait@manchester.ac.uk>
2019-01-07 16:42:50 +0000

    flake8 blank lines, correct executable type

--- spinnaker_graph_front_end/examples/test_extra_monitor_core_data_extraction/sdram_writer.py
+++ spinnaker_graph_front_end/examples/test_extra_monitor_core_data_extraction/sdram_writer.py
@@ -38,9 +38,7 @@ class SDRAMWriter(
             self.CONFIG_REGION_SIZE))
 
     def get_binary_start_type(self):
-        return ExecutableType.SYNC
-#        return ExecutableType.USES_SIMULATION_INTERFACE
-
+        return ExecutableType.USES_SIMULATION_INTERFACE
 
     def generate_machine_data_specification(
             self, spec, placement, machine_graph, routing_info, iptags,
