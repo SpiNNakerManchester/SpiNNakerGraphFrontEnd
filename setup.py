21cc75956e559d5dba8e26bb5fca94947db45d01
christian-B <christian.brenninkmeijer@manchester.ac.uk>
2019-07-16 10:27:44 +0100

    added or updated classifiers

--- setup.py
+++ setup.py
@@ -35,6 +35,27 @@ setup(
     description="Front end to the SpiNNaker tool chain which uses a "
                 "basic graph",
     url="https://github.com/SpiNNakerManchester/SpiNNakerGraphFrontEnd",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+
+        "Natural Language :: English",
+
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
     packages=packages,
     package_data=package_data,
     install_requires=['SpiNNUtilities >= 1!4.0.1, < 1!5.0.0',
