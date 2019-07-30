b990c0b343a51cc355c23abc78946d52f523aaf5
Konstantinos Alvertis <konstantinos.alvertis@gmail.com>
2019-07-30 12:41:56 +0100

    introduce if case for the matMul , add_broadcast and Const vertices

--- spinnaker_graph_front_end/examples/TensorSample/LoadMnistWithNumpyRawFile.py
+++ spinnaker_graph_front_end/examples/TensorSample/LoadMnistWithNumpyRawFile.py
@@ -1,16 +1,30 @@
-#!/usr/bin/env python
-# coding: utf-8
-
+from pacman.model.graphs.machine import MachineEdge
+from tensorflow.python.framework import tensor_util
+import logging
+import os
 import numpy as np
-import tensorflow as tf
-import tensorflow.compat.v1 as tf
+import spinnaker_graph_front_end as front_end
 from spinnaker_graph_front_end.examples.TensorSample.mat_mul_vertex_non_dynamic import (MatMulVertexND)
+from spinnaker_graph_front_end.examples.TensorSample.add_broadcast_vertex_non_dynamic import (AddBroadcastND)
+from spinnaker_graph_front_end.examples.TensorSample.const_tensor_vertex_non_dynamic import (ConstTensorVertexND)
+from spinnaker_graph_front_end.examples.TensorSample.softmax_vertex_non_dynamic import (SoftmaxND)
+from spinnaker_graph_front_end.examples.TensorSample.log_vertex_non_dynamic import (LogND)
+from spinnaker_graph_front_end.examples.TensorSample.mul_broadcast_vertex_non_dynamic import (MulBroadcastND)
+from spinnaker_graph_front_end.examples.TensorSample.reduce_sum_non_dynamic import (ReduceSum)
+from spinnaker_graph_front_end.examples.TensorSample.const_scalar_vertex import (ConstScalarVertex)
+from spinnaker_graph_front_end.examples.TensorSample.tf_fill_vertex import (FillVertex)
+from spinnaker_graph_front_end.examples.TensorSample.const_empty_vertex import (ConstEmptyVertex)
+from spinnaker_graph_front_end.examples.TensorSample.tf_neg_vertex import (NegVertex)
 
+
+import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 np.random.seed(0)
 
+logger = logging.getLogger(__name__)
 
-
+front_end.setup(n_chips_required=1, model_binary_folder=os.path.dirname(__file__))
+tf.set_random_seed(0)
 
 def load_data(path):
     with np.load(path) as f:
@@ -37,9 +51,15 @@ def convert_to_one_hot(y):
     result[np.arange(y.size), y] = 1
     return result
 
+
 def from_tensor_get_operation_name(tensor_name):
     return tensor_name.split(sep=':')[0]
 
+
+def get_input_shapes(name):
+    sp1 = graph._nodes_by_name[name]._inputs._inputs[0].get_shape().as_list()
+    sp2 = graph._nodes_by_name[name]._inputs._inputs[1].get_shape().as_list()
+    return sp1, sp2
 
 
 # Parameters
@@ -118,6 +138,9 @@ def store_input_node_names (name):
     inputs[name] = current_inputs
 
 
+final_weight=0
+final_bias=0
+
 for i in range(training_epochs):
 
     batch_X, batch_Y = next_batch(batch_size, x_train, y_train)
@@ -143,28 +166,92 @@ for i in range(training_epochs):
         final_weight = sess.run(W)
         final_bias = sess.run(b)
 
-        one_digit = np.reshape(x_test[0], (-1, 784))
+one_digit = np.reshape(x_test[0], (-1, 784))
+one_digit = tf.constant(one_digit, dtype = tf.float32)
 
-        one_digit = one_digit.astype(np.float32)
+one_digit_label = y_test[0]
+one_digit_label = tf.constant(one_digit_label, dtype = tf.float32)
 
-        one_digit_label = y_test[0]
-        one_digit_label = one_digit_label.astype(np.float32)
+final_weight = tf.constant(final_weight, dtype = tf.float32)
+final_bias = tf.constant(final_bias, dtype = tf.float32)
 
-        prediction = tf.matmul(one_digit, final_weight) + final_bias
-        pred_max = tf.argmax(input=prediction, axis=1)
-        test_max = tf.argmax(input=one_digit_label, axis=0)
 
-        result = tf.equal(pred_max, test_max)
+prediction = tf.matmul(one_digit, final_weight) + final_bias
+pred_max = tf.argmax(input=prediction, axis=1)
+test_max = tf.argmax(input=one_digit_label, axis=0)
 
-        # Add Vertices
-        for n in tf.get_default_graph().as_graph_def().node:
-            # print('node id :', n_id, 'and name:', graph._nodes_by_id[n_id].name)
-            if n.name not in training_nodes:
+result = tf.equal(pred_max, test_max)
 
-                if n.name == 'MatMul':
-                    shape1 = graph._nodes_by_name[n.name]._inputs._inputs[0].get_shape().as_list()
-                    shape2 = graph._nodes_by_name[n.name]._inputs._inputs[1].get_shape().as_list()
-                    vertices[n_id] = MatMulVertexND("{} vertex ".format(n.name), shape1, shape2)
+const = {}
+for n in tf.get_default_graph().as_graph_def().node:
+    if n.op == 'Const':
+        if not n.attr["value"].tensor.tensor_shape.dim:
+            if len(n.attr.get('value').tensor.float_val):
+                const[n.name] = n.attr.get('value').tensor.float_val[0]
+            if len(n.attr.get('value').tensor.int_val):
+                const[n.name] = n.attr.get('value').tensor.int_val[0]
+        else:
+            const[n.name] = tensor_util.MakeNdarray(n.attr['value'].tensor)
+
+for n in tf.get_default_graph().as_graph_def().node:
+    print('name:', n.name)
+
+    # Convert only the testing nodes
+    if n.name not in training_nodes:
+
+        if 'MatMul' in n.name:
+            shape1, shape2 = get_input_shapes(n.name)
+            vertices[n.name] = MatMulVertexND("{} vertex ".format(n.name), shape1, shape2)
+
+        if 'add' in n.name:
+            shape1, shape2 = get_input_shapes(n.name)
+            vertices[n.name] = AddBroadcastND("{} vertex ".format(n.name), shape1, shape2)
+
+        elif 'Const' in n.name:
+            vertices[n.name] = ConstTensorVertexND("{} vertex ".format(n.name), const[n.name])
+
+            store_input_node_names(n.name)
+            front_end.add_machine_vertex_instance(vertices[n.name])
+
+    else:
+        continue
+
+
+
+# Add Edges
+for name in vertices:
+    # Check if this vertex has inputs nodes
+    if name in inputs:
+        # Iterate over input ids of the nodes
+        for n in inputs[name]:
+            # add the edge
+            front_end.add_machine_edge_instance(MachineEdge(vertices[n], vertices[name],
+                                                            label=n + ': to ' + name),
+                                                "OPERATION_PARTITION")
+
+sess.close()
+
+print("run simulation")
+front_end.run(1)
+
+placements = front_end.placements()
+txrx = front_end.transceiver()
+
+print("read SDRAM after run")
+for placement in sorted(placements.placements,
+                        key=lambda p: (p.x, p.y, p.p)):
+
+    if isinstance(placement.vertex, ConstTensorVertexND):
+        const_value = placement.vertex.read(placement, txrx)
+        logger.info("CONST {}, {}, {} > {}".format(
+            placement.x, placement.y, placement.p, const_value))
+
+    if isinstance(placement.vertex, MatMulVertexND):
+        oper_results = placement.vertex.read(placement, txrx)
+        logger.info("Mat Mul {}, {}, {} > {}".format(
+            placement.x, placement.y, placement.p, oper_results))
+
+front_end.stop()
 
         # # MatMul
         # shape1 = [1, 784]
