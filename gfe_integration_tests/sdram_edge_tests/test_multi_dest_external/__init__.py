112da249850922d55ac9427a6369dcbf83264c79
alan-stokes <alan.barry.stokes@gmail.com>
2020-10-29 11:08:27 +0000

    dest external test

---
+++ gfe_integration_tests/sdram_edge_tests/test_multi_dest_external/__init__.py
@@ -0,0 +1,7 @@
+from .sdram_machine_vertex import SDRAMMachineVertex
+from .sdram_splitter import SDRAM_Splitter
+from .test_sdram_multiple_dests_edges import (
+    TestMultiDestSDRAMEdgeBetweenTwoAppVerts)
+
+__all__ = ["SDRAM_Splitter", "SDRAMMachineVertex",
+           "TestMultiDestSDRAMEdgeBetweenTwoAppVerts"]
---
+++ gfe_integration_tests/sdram_edge_tests/test_multi_dest_external/test_sdram_multiple_dests_edges.py
@@ -0,0 +1,51 @@
+# Copyright (c) 2020-2021 The University of Manchester
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import unittest
+
+from fec_integration_tests.interface.interface_functions.\
+    simple_test_vertex import SimpleTestVertex
+from gfe_integration_tests.sdram_edge_tests.test_multi_dest_external \
+    import SDRAM_Splitter
+from pacman.model.graphs.application import ApplicationEdge
+from gfe_integration_tests.sdram_edge_tests import test_multi_dest_external
+import spinnaker_graph_front_end as sim
+from pacman.model.graphs.machine.outgoing_edge_partitions import \
+    DestinationSegmentedSDRAMMachinePartition
+
+
+class TestMultiDestSDRAMEdgeBetweenTwoAppVerts(unittest.TestCase):
+
+    def setup(self):
+        sim.setup(model_binary_module=test_multi_dest_external)
+        vertex_1 = SimpleTestVertex(2, fixed_sdram_value=20)
+        vertex_1.splitter = SDRAM_Splitter(
+            DestinationSegmentedSDRAMMachinePartition)
+        vertex_2 = SimpleTestVertex(2, fixed_sdram_value=20)
+        vertex_2.splitter = SDRAM_Splitter(
+            DestinationSegmentedSDRAMMachinePartition)
+        vertex_3 = SimpleTestVertex(2, fixed_sdram_value=20)
+        vertex_3.splitter = SDRAM_Splitter(
+            DestinationSegmentedSDRAMMachinePartition)
+        sim.add_vertex_instance(vertex_1)
+        sim.add_vertex_instance(vertex_2)
+        sim.add_vertex_instance(vertex_3)
+        sim.add_application_edge_instance(
+            ApplicationEdge(vertex_1, vertex_2), "sdram")
+        sim.add_application_edge_instance(
+            ApplicationEdge(vertex_1, vertex_3), "sdram")
+        sim.run(100)
+
+    def test_local_verts_go_to_local_lpgs(self):
+        self.setup()
