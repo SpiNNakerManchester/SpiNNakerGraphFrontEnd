c109aa3cf0793554791e704fde76633a53555efd
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2024-12-16 09:28:43 +0000

    return None

--- gfe_examples/hello_world_untimed/hello_world_vertex.py
+++ gfe_examples/hello_world_untimed/hello_world_vertex.py
@@ -88,8 +88,8 @@ class HelloWorldVertex(
         return VariableSDRAM(fixed, variable)
 
     @overrides(AbstractGeneratesDataSpecification.generate_data_specification)
-    def generate_data_specification(
-            self, spec: DataSpecificationGenerator, placement: Placement):
+    def generate_data_specification(self, spec: DataSpecificationGenerator,
+                                    placement: Placement) -> None:
         # pylint: disable=arguments-differ
 
         # Generate the system data region for simulation .c requirements
