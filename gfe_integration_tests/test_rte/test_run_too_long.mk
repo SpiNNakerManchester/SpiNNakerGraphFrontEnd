51def276de9c5b1e464f087f7695890fdd38f9fe
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2025-11-20 09:08:30 +0000

    char database id

--- gfe_integration_tests/test_rte/test_run_too_long.mk
+++ gfe_integration_tests/test_rte/test_run_too_long.mk
@@ -21,7 +21,7 @@ APP = test_run_too_long
 SOURCES = test_run_too_long.c
 
 APP_OUTPUT_DIR := $(abspath $(dir $(abspath $(lastword $(MAKEFILE_LIST)))))/
-DATABASE_ID = 3
+DATABASE_ID = G
 
 # The spinnaker_tools standard makefile
 include $(SPINN_DIRS)/make/local.mk
