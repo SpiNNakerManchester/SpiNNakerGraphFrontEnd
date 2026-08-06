10efcc0e0f06bd1cbbdeafd0d27eee4c5af9c717
christian-B <christian.brenninkmeijer@manchester.ac.uk>
2026-08-06 08:38:02 +0100

    flake8

--- gfe_examples/Conways/partitioned_example_b_no_vis_buffer/conways_basic_cell.py
+++ gfe_examples/Conways/partitioned_example_b_no_vis_buffer/conways_basic_cell.py
@@ -30,10 +30,11 @@ from spinn_front_end_common.data import FecDataView
 from spinn_front_end_common.interface.buffer_management.buffer_models import (
     AbstractReceiveBuffersToHost,
 )
-from spinn_front_end_common.interface.buffer_management.recording_utilities import (
-    get_recording_data_constant_size,
-    get_recording_header_size,
-)
+from spinn_front_end_common.interface.buffer_management. \
+    recording_utilities import (
+        get_recording_data_constant_size,
+        get_recording_header_size,
+    )
 from spinn_front_end_common.interface.ds import DataSpecificationGenerator
 from spinn_front_end_common.utilities.constants import (
     BYTES_PER_WORD,
